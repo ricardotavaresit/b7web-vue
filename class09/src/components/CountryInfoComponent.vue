ba83e35ffcd7ddd01bf6b6ee7bb005dba348fa0f
rtavares <ricardotavaresit@gmail.com>
2022-06-17 12:41:15 +0100

    update path

---
+++ class09/src/components/CountryInfoComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{ title }}</div>
+</template>
+ 
+
+ <script>
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  data() {
+    return {
+      title: "cenas",
+    };
+  },
+});
+</script>
+ 
